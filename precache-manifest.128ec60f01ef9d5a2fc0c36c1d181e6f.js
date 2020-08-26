b97d2e3e5c405c58405e9be00ce587b3d30d7f9c
unknown <samipg97@gmail.com>
2020-08-26 13:12:06 +0530

    Updates

---
+++ precache-manifest.128ec60f01ef9d5a2fc0c36c1d181e6f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad074674efbe5e8b8d3c1f4fa202b780",
+    "url": "/beingveg/index.html"
+  },
+  {
+    "revision": "d223457cbb8b0856e904",
+    "url": "/beingveg/static/css/main.9ade6a34.chunk.css"
+  },
+  {
+    "revision": "60b43bb0b1196733d561",
+    "url": "/beingveg/static/js/2.8b3c9042.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beingveg/static/js/2.8b3c9042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d223457cbb8b0856e904",
+    "url": "/beingveg/static/js/main.f1a4deab.chunk.js"
+  },
+  {
+    "revision": "a941463215f3cdce0ea2",
+    "url": "/beingveg/static/js/runtime-main.1eb4568f.js"
+  },
+  {
+    "revision": "f80c732c0649a33fc676cf2d94826f12",
+    "url": "/beingveg/static/media/back-img.f80c732c.jpg"
+  },
+  {
+    "revision": "75d6f02257eaebb7b9ff08716edd7bde",
+    "url": "/beingveg/static/media/berlin.75d6f022.jpg"
+  },
+  {
+    "revision": "b95839c2c6487c5eee3ecd76ebb8e1c0",
+    "url": "/beingveg/static/media/bottom-bar.b95839c2.png"
+  },
+  {
+    "revision": "aeadf4edbeef10d5cca617b91e59a8a3",
+    "url": "/beingveg/static/media/dining_img.aeadf4ed.jpg"
+  },
+  {
+    "revision": "9083f420a37f41578c728569722dbb84",
+    "url": "/beingveg/static/media/event_img.9083f420.svg"
+  },
+  {
+    "revision": "e57aec5e4a4688d8603b83b67cdb856d",
+    "url": "/beingveg/static/media/logo.e57aec5e.svg"
+  },
+  {
+    "revision": "85e18a0a851e039e51b87e0d32439537",
+    "url": "/beingveg/static/media/london.85e18a0a.jpg"
+  },
+  {
+    "revision": "5998c996f35fd788b142f181996588b7",
+    "url": "/beingveg/static/media/los-angeles.5998c996.jpg"
+  },
+  {
+    "revision": "208eaa217d1154ccab0c554f639367ac",
+    "url": "/beingveg/static/media/new-york-city.208eaa21.jpg"
+  },
+  {
+    "revision": "f1cfd6ac5056788cff125a6081e8e5e1",
+    "url": "/beingveg/static/media/user.f1cfd6ac.png"
+  }
+]);
